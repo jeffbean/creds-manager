857a216847bc55b4924efd5b91805768e5400035
Jeff Bean <bean@uber.com>
2018-10-06 20:13:03 -0700

    Change the tooling to match refactor

--- tools/ssh-helper/main.go
+++ tools/ssh-helper/main.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
-	"net"
-	"os"
-
-	"golang.org/x/crypto/ed25519"
+	"path/filepath"
+	"time"
 
 	"github.com/jeffbean/creds-manager/ssh"
 	"github.com/jessevdk/go-flags"
-	"golang.org/x/crypto/ssh/agent"
 )
 
 // Flags are the commands command line flags
 type Flags struct {
-	Name string `long:"name" required:"true" description:"The name of the ssh-key to manage." positional-arg:"true"`
+	OutputFile   flags.Filename `short:"f" description:"Output file"`
+	AgentTimeout time.Duration  `short:"t" long:"agentTime" description:"The amount of time to live in the ssh agent" default:"20h"`
+	Comment      string         `short:"C" long:"comment" description:"An ssh key comment."`
+
+	Args struct {
+		Name string
+	} `description:"The name of the ssh-key to manage." positional-args:"yes" required:"yes" `
 }
 
 func main() {
@@ -31,30 +35,32 @@ func run() error {
 	if _, err := parser.Parse(); err != nil {
 		return err
 	}
-	// ED doesn't use bitsize
-	keyPair, err := ssh.GenerateSSHKeyPair("", ssh.ED25519, 0)
+
+	// if not specified we just make a temp dir and put it there for you to do what you will with it
+	if f.OutputFile == "" {
+		tmpDir, err := ioutil.TempDir("", "ssh-helper")
+		if err != nil {
+			return err
+		}
+
+		f.OutputFile = flags.Filename(filepath.Join(tmpDir, f.Args.Name))
+	}
+
+	log.Printf("f: %#v", f)
+	// ED25519 doesn't use bitsize
+	keyPair, err := ssh.GenerateSSHKeyPair(ssh.ED25519, 0)
 	if err != nil {
 		return fmt.Errorf("failed to create ssh keys: %v", err)
 	}
-	if err := AddKeyToAgent(keyPair); err != nil {
+
+	if err := ssh.WriteKeysToDisk(string(f.OutputFile), keyPair, f.Comment); err != nil {
+		return fmt.Errorf("failed to write ssh files: %v", err)
+	}
+
+	comment := fmt.Sprintf("[ssh-helper %s]", f.Comment)
+	if err := ssh.AddKeyToAgent(keyPair, ssh.WithTimeout(f.AgentTimeout), ssh.WithComment(comment)); err != nil {
 		return fmt.Errorf("failed to add key to ssh agent: %v", err)
 	}
 
 	return nil
-}
-
-func AddKeyToAgent(kp *ssh.KeyPair) error {
-	sshAgent, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK"))
-	if err != nil {
-		return err
-	}
-	sshClient := agent.NewClient(sshAgent)
-	addKey := agent.AddedKey{
-		LifetimeSecs: 120,
-	}
-	switch privateKey := kp.PrivateKey.(type) {
-	case ed25519.PrivateKey:
-		addKey.PrivateKey = &privateKey
-	}
-	return sshClient.Add(addKey)
 }
